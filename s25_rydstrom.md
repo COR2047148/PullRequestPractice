9a99f0c32ca4202b5b00622c7ac62d8e47cfcf4f
gitcryds <COR2047148@maricopa.edu>
2025-03-28 13:26:59 -0700

    Commit 1

---
+++ s25_rydstrom.md
@@ -0,0 +1,7 @@
+
+# The Dark Knight Movie Quote
+
+> You Either Die a Hero, or You Live Long Enough to See Yourself Become the Villain.
+
+\- Harvey Dent
+(The Dark Knight, 2008)
---
+++ s25_rydstrom.py
@@ -0,0 +1,12 @@
+"""
+Corey Rydstrom
+Module 08 Programming Project
+
+A Python file with a quote from "The Dark Knight"
+"""
+
+the_dark_knight = ("You Either Die a Hero, or You Live Long Enough to "
+                   "See Yourself Become the Villain. -Harvey Dent "
+                   "(The Dark Knight, 2008)")
+
+print(the_dark_knight)
